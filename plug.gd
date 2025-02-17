fd22cdd958fbb1e434722477f3d07122b259ff01
adrian leguizamon <adrian.luis.leguizamon@gmail.com>
2025-02-17 13:28:24 -0300

    build(gd-plug): actualicé la url de ModManager

--- plug.gd
+++ plug.gd
@@ -12,7 +12,7 @@ func _plugging():
 	)
 	plug("MarcoFazioRandom/Virtual-Joystick-Godot")
 	plug(
-		"git@gitlab.com:adrian.luis.leguizamon/mod_manager.git",
+		"adrianlegui/mod_manager",
 		{"exclude": ["addons/mod_manager/test", "addons/mod_manager/schema"], "tag": "v0.10.0"}
 	)
 	plug("poingstudios/godot-admob-plugin", {"tag": "v3.1.1"})
