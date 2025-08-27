75830d8e9a32ebb3877adf26d4da8956ab0279c0
adrian <adrian.luis.leguizamon@gmail.com>
2025-08-27 17:20:43 -0300

    refactor(main.gd): espera a EnteModManager termine de cargar antes de continuar

--- src/runner_3d/gd/main/main.gd
+++ src/runner_3d/gd/main/main.gd
@@ -3,6 +3,8 @@ extends Node
 
 func _ready() -> void:
 	EnteModManager.start()
+	await EnteModManager.finished
+
 	var savegame_path: String = EnteModManager.get_path_to_savegame(
 		Runner3DConst.SAVEGAME_NAME
 	)
@@ -11,4 +13,6 @@ func _ready() -> void:
 		EnteModManager.load_savegame(Runner3DConst.SAVEGAME_NAME)
 	else:
 		EnteModManager.start_game()
+
+	await EnteModManager.started_game
 	queue_free()
